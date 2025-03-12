cb95e977ccc45c5a354a8aa2588afc72a01cd5c0
Cody Scott <scotty_p@Mac.lan>
2025-03-11 23:04:44 -0400

    Update: blocked

--- src/index.js
+++ src/index.js
@@ -94,10 +94,13 @@ io.on('connection', (socket) => {
                 User.findById(socket.user.id).select('blocked'),
             ]);
 
-            console.log([otherUser, currentUser])
-            // If the sender is blocked by the receiver, stop execution
+            console.log([otherUser, currentUser]);
+
+            // If the sender is blocked by the receiver, stop execution and emit 'messageBlocked'
             if (otherUser.blocked?.includes(socket.user.id)) {
                 console.log(`Message blocked: ${socket.user.id} is blocked by ${receiverId}`);
+                // Emit the messageBlocked event to the sender
+                socket.emit('messageBlocked', { error: 'User is no longer accepting messages from this account.' });
                 return;
             }
 
@@ -140,9 +143,6 @@ io.on('connection', (socket) => {
             console.error('Error sending message:', err);
         }
     });
-
-
-
 
     socket.on('markMessagesAsRead', async (conversationId) => {
         try {
